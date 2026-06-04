c6bb1684c425f8235183ead1677b9ed12c4fa094
alumno <alumno@Instituto.local>
2026-06-04 09:08:04 -0300

    4 de junio

--- Trabajo_practico_5/app.js
+++ Trabajo_practico_5/app.js
@@ -1,7 +1,10 @@
 let numero1 = 25;
 let numero2 = 23;
-
-let resultado = numero1 + numero2;
-
-console.log(resultado)
-alert(resultado)
+let suma = numero1 + numero2; 
+let resta = numero1 - numero2; 
+let multiplicacion = numero1 * numero2; 
+let division = numero1 / numero2;
+alert(suma);
+alert(resta);
+alert(multiplicacion);
+alert(division);
